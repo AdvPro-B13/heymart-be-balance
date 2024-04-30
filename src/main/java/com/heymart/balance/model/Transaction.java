63601c43cb3af79468c4f750e72fa43a8da9a893
DhafinFK <dhafin.kamal@gmail.com>
2024-04-30 22:57:02 +0700

    [GREEN] implemented transaction model

---
+++ src/main/java/com/heymart/balance/model/Transaction.java
@@ -0,0 +1,32 @@
+package com.heymart.balance.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Transaction {
+    private UUID id;
+    private UUID ownerId;
+    private Date transactionDate;
+    private double amount;
+    private TransactionType transactionType;
+
+    public enum TransactionType {
+        TOPUP, WITHDRAWAL
+    }
+
+    public Transaction() {
+
+    }
+
+    public Transaction(UUID ownerId, Date transactionDate, double amount, TransactionType transactionType) {
+        this.ownerId = ownerId;
+        this.transactionDate = transactionDate;
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+}
