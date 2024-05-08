22a2a98488141e54896177a65102de0f7b891028
DhafinFK <dhafin.kamal@gmail.com>
2024-05-08 11:16:29 +0700

    [REFACTOR] changed cors for api testing

--- src/main/java/com/heymart/balance/controller/SupermarketBalanceController.java
+++ src/main/java/com/heymart/balance/controller/SupermarketBalanceController.java
@@ -5,13 +5,7 @@ import java.util.UUID;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.heymart.balance.dto.AmountDTO;
 import com.heymart.balance.model.Supermarket;
@@ -19,6 +13,7 @@ import com.heymart.balance.model.SupermarketBalance;
 import com.heymart.balance.service.SupermarketBalanceService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/supermarket-balance")
 public class SupermarketBalanceController {
 
