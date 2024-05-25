a904987b53df73e12f7081234335633c22d113cc
DhafinFK <dhafin.kamal@gmail.com>
2024-05-25 13:36:46 +0700

    [FIX] committed missing addition for service implementation

--- src/main/java/com/heymart/balance/service/BalanceServiceImpl.java
+++ src/main/java/com/heymart/balance/service/BalanceServiceImpl.java
@@ -1,5 +1,6 @@
 package com.heymart.balance.service;
 
+import com.heymart.balance.exceptions.BalanceNotFoundException;
 import com.heymart.balance.factory.BalanceFactory;
 import com.heymart.balance.factory.SupermarketBalanceFactory;
 import com.heymart.balance.factory.UserBalanceFactory;
@@ -8,12 +9,14 @@ import com.heymart.balance.model.Transaction;
 import com.heymart.balance.repository.BalanceRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class BalanceServiceImpl implements BalanceService{
@@ -31,80 +34,103 @@ public class BalanceServiceImpl implements BalanceService{
         this.supermarketBalanceFactory = new SupermarketBalanceFactory();
     }
 
+    @Async("taskExecutor")
     @Transactional
-    public Balance createBalance(UUID ownerId, Balance.OwnerType ownerType) {
+    public CompletableFuture<Balance> createBalance(UUID ownerId, Balance.OwnerType ownerType) {
         Optional<Balance> existingBalance = balanceRepository.findByOwnerId(ownerId);
         if (existingBalance.isPresent()) {
             throw new IllegalArgumentException("Owner already has a balance");
         }
         BalanceFactory factory = (ownerType == Balance.OwnerType.USER) ? userBalanceFactory : supermarketBalanceFactory;
         Balance balance = factory.createBalance(ownerId, ownerType);
-        return balanceRepository.save(balance);
+        balance = balanceRepository.save(balance);
+        return CompletableFuture.completedFuture(balance);
     }
 
+    @Async("taskExecutor")
     @Override
-    public Optional<Balance> findById(UUID id) {
-        return balanceRepository.findById(id);
+    public CompletableFuture<Optional<Balance>> findById(UUID id) {
+        return CompletableFuture.completedFuture(balanceRepository.findById(id));
     }
 
+    @Async("taskExecutor")
     @Override
-    public Optional<Balance> findByOwnerId(UUID ownerId) {
-        return balanceRepository.findByOwnerId(ownerId);
+    public CompletableFuture<Optional<Balance>> findByOwnerId(UUID ownerId) {
+        return CompletableFuture.completedFuture(balanceRepository.findByOwnerId(ownerId));
     }
 
+    @Async("taskExecutor")
     @Transactional
-    public Optional<Balance> topUp(UUID ownerId, double amount) {
-        Optional<Balance> balance = findByOwnerId(ownerId);
-        return balance.map(b -> {
-            b.setBalance(b.getBalance() + amount);
+    public CompletableFuture<Optional<Balance>> topUp(UUID ownerId, double amount) {
+        Optional<Balance> balance = balanceRepository.findByOwnerId(ownerId);
+        if (balance.isEmpty()) {
+            throw new BalanceNotFoundException("Balance not found for owner Id" + ownerId.toString());
+        }
+
+        Balance b = balance.get();
+
+        b.setBalance(b.getBalance() + amount);
+        balanceRepository.save(b);
+
+        Transaction createdTransaction = new Transaction();
+        createdTransaction.setOwnerId(ownerId);
+        createdTransaction.setOwnerType(Transaction.OwnerType.valueOf(b.getOwnerType().toString()));
+        createdTransaction.setAmount(amount);
+        createdTransaction.setTransactionType(Transaction.TransactionType.TOPUP);
+        createdTransaction.setBalance(b);
+        transactionService.createTransaction(createdTransaction);
+        return CompletableFuture.completedFuture(balanceRepository.findById(b.getId()));
+
+    }
+
+    @Async("taskExecutor")
+    @Transactional
+    public CompletableFuture<Optional<Balance>> withdraw(UUID ownerId, double amount) {
+        Optional<Balance> balance = balanceRepository.findByOwnerId(ownerId);
+
+        if (balance.isEmpty()) {
+            throw new BalanceNotFoundException("Balance not found for owner Id" + ownerId.toString());
+        }
+
+        Balance b = balance.get();
+
+        if (b.getBalance() >= amount) {
+            b.setBalance(b.getBalance() - amount);
+            balanceRepository.save(b);
 
             Transaction createdTransaction = new Transaction();
             createdTransaction.setOwnerId(ownerId);
             createdTransaction.setOwnerType(Transaction.OwnerType.valueOf(b.getOwnerType().toString()));
             createdTransaction.setAmount(amount);
-            createdTransaction.setTransactionType(Transaction.TransactionType.TOPUP);
+            createdTransaction.setTransactionType(Transaction.TransactionType.WITHDRAWAL);
             createdTransaction.setBalance(b);
             transactionService.createTransaction(createdTransaction);
-
-            return balanceRepository.save(b);
-        });
+            return CompletableFuture.completedFuture(balanceRepository.findById(b.getId()));
+        } else {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
     }
 
+    @Async("taskExecutor")
     @Transactional
-    public Optional<Balance> withdraw(UUID ownerId, double amount) {
-        Optional<Balance> balance = findByOwnerId(ownerId);
-        return balance.map(b -> {
-            if (b.getBalance() >= amount) {
-                b.setBalance(b.getBalance() - amount);
-
-                Transaction createdTransaction = new Transaction();
-                createdTransaction.setOwnerId(ownerId);
-                createdTransaction.setOwnerType(Transaction.OwnerType.valueOf(b.getOwnerType().toString()));
-                createdTransaction.setAmount(amount);
-                createdTransaction.setTransactionType(Transaction.TransactionType.WITHDRAWAL);
-                createdTransaction.setBalance(b);
-                transactionService.createTransaction(createdTransaction);
-
-                return balanceRepository.save(b);
-            } else {
-                throw new IllegalArgumentException("Insufficient funds");
-            }
-        });
-    }
-
-    @Transactional
-    public List<Balance> checkout(UUID userId, UUID supermarketId, double amount) {
+    public CompletableFuture<List<Balance>> checkout(UUID userId, UUID supermarketId, double amount) {
         Optional<Balance> userBalance = balanceRepository.findByOwnerId(userId);
         Optional<Balance> supermarketBalance = balanceRepository.findByOwnerId(supermarketId);
+        List<Balance> response = new ArrayList<>();
         if (userBalance.isPresent() && supermarketBalance.isPresent()) {
-            List<Balance> response = new ArrayList<>();
-            this.withdraw(userId, amount);
+            // Execute withdraw and topUp asynchronously and block until they complete
+            this.withdraw(userId, amount).join();
             this.topUp(supermarketId, amount);
-            response.add(userBalance.get());
-            response.add(supermarketBalance.get());
-            return response;
+
+            // Reload balances from the database to get the updated values
+            Optional<Balance> updatedUserBalance = balanceRepository.findByOwnerId(userId);
+            Optional<Balance> updatedSupermarketBalance = balanceRepository.findByOwnerId(supermarketId);
+
+            response.add(updatedUserBalance.get());
+            response.add(updatedSupermarketBalance.get());
+            return CompletableFuture.completedFuture(response);
         }
 
-        return new ArrayList<>();
+        throw new BalanceNotFoundException("Balance not found for owner or supermarket");
     }
 }
--- src/main/java/com/heymart/balance/service/TransactionService.java
+++ src/main/java/com/heymart/balance/service/TransactionService.java
@@ -5,9 +5,10 @@ import com.heymart.balance.model.Transaction;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public interface TransactionService {
-    public Transaction createTransaction(Transaction transaction);
-    public Optional<Transaction> findById(UUID id);
-    public List<Transaction> findByOwnerId(UUID ownerId);
+    public CompletableFuture<Transaction> createTransaction(Transaction transaction);
+    public CompletableFuture<Optional<Transaction>> findById(UUID id);
+    public CompletableFuture<List<Transaction>> findByOwnerId(UUID ownerId);
 }
