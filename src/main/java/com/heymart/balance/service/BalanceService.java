5421f380b9573ef8f574537a7a73de7650273cee
DhafinFK <dhafin.kamal@gmail.com>
2024-05-25 13:35:15 +0700

    [GREEN] implemented balance service

--- src/main/java/com/heymart/balance/service/BalanceService.java
+++ src/main/java/com/heymart/balance/service/BalanceService.java
@@ -5,12 +5,13 @@ import com.heymart.balance.model.Balance;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public interface BalanceService {
-    public Balance createBalance(UUID ownerId, Balance.OwnerType ownerType);
-    public Optional<Balance> findById(UUID id);
-    public Optional<Balance> findByOwnerId(UUID ownerId);
-    public Optional<Balance> topUp(UUID ownerId, double amount);
-    public Optional<Balance> withdraw(UUID ownerId, double amount);
-    public List<Balance> checkout(UUID userId, UUID supermarketId, double amount);
+    public CompletableFuture<Balance> createBalance(UUID ownerId, Balance.OwnerType ownerType);
+    public CompletableFuture<Optional<Balance>> findById(UUID id);
+    public CompletableFuture<Optional<Balance>> findByOwnerId(UUID ownerId);
+    public CompletableFuture<Optional<Balance>> topUp(UUID ownerId, double amount);
+    public CompletableFuture<Optional<Balance>> withdraw(UUID ownerId, double amount);
+    public CompletableFuture<List<Balance>> checkout(UUID userId, UUID supermarketId, double amount);
 }
